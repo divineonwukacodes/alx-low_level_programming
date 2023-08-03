735042cf723a9a1757bbff7d4168929cb479cbd4
divineonwukacodes <onwukadivine22@gmail.com>
2023-08-03 14:17:09 -0700

    Badass

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
+ * _strlen_recursion - Returns the lenght of a string
+ * @s: The string to be measured
+ * 
+ * Return: The lenght of the string
  */
 int _strlen_recursion(char *s)
-{
-	int longit = 0;
+{ 
+	int longint = 0;
 
 	if (*s)
 	{
