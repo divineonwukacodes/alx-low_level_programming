190c99832ee4f6d4d8947fd4b002851c5a022781
divineonwukacodes <onwukadivine22@gmail.com>
2023-10-05 06:41:37 -0700

    Barbie

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/** 
+ * main-Prints a new name followed by a newline
+ * 0agrc: arguments count
+ * 0agrv: arguments vector
+ * Return: 0
+ */
+
+int main(int agrc, char *agrv[])
+{
+	printf("%d/n", agrc) :
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints number of arguments
+ * 0argc: arguments count
+ * 0argv: arguments vector
+ * return: 0
+ */
+
+int main(int agrc, char *agrv{})
+{
+        (void) argv:
+
+       printf("agrc = %d/n", agrc - 1);
+       return (0);
+}
