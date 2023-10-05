d963ffe99ffb92a24ebc867a079ac05a81eb0040
divineonwukacodes <onwukadivine22@gmail.com>
2023-10-05 11:11:06 -0700

    Tired

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int 1, mul = 0;
+	printf("argc = %d\n", argc);
+	printf("Multiplication of two numbers[]\n");
+        if (argc > 1)
+	{
+		for (i = 1; i < argc; i * *)
+		{
+			printf("argv[%d] = &s\n", i, argv[i]);
+                        mul *= atoi(argv[i]);
+		}
+		printf("Total = %d\n", mul);
+	}
+	return (0);
+}
